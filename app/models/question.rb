d501e328fe63316c5184471ab209dc5393d52c17
rhoen+charles <rhoen+charles@appacademy.io>
2015-04-09 18:09:28 -0400

    User instance method returns completed polls. Bonus, Yay

--- app/models/question.rb
+++ app/models/question.rb
@@ -21,4 +21,48 @@ class Question < ActiveRecord::Base
     source: :responses
   )
 
+  def results
+    results = {}
+    answer_choices.each do |choice|
+      results[choice.text] = choice.responses.count
+    end
+
+    results
+  end
+
+  def opt_results
+    results = {}
+    answer_choices.includes(:responses).each do |choice|
+      results[choice.text] = choice.responses.length
+    end
+
+    results
+  end
+
+  def sql_results
+    self.class.connection.execute(<<-SQL
+      SELECT
+        answer_choices.*, COUNT(responses.answer_choice_id)
+      FROM
+        answer_choices
+      LEFT OUTER JOIN
+        responses ON responses.answer_choice_id = answer_choices.id
+      GROUP BY
+        answer_choice_id
+
+      SQL
+    )
+  end
+
+  def ar_results
+    results = AnswerChoice
+      .select("answer_choices.*, COUNT(responses.answer_choice_id) as response_count")
+      .joins("LEFT OUTER JOIN responses ON responses.answer_choice_id = answer_choices.id")
+      .group("answer_choice_id")
+
+    results.map do |result|
+      [result.text, result.response_count]
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,87 @@ class User < ActiveRecord::Base
     primary_key: :id
   )
 
+  def completed_polls
+    Poll.find_by_sql ["
+    SELECT
+       polls.*,COUNT(DISTINCT questions.id), COUNT(user_responses.user_id)
+    FROM
+      polls
+    JOIN
+      questions ON questions.poll_id = polls.id
+    JOIN
+      answer_choices ON answer_choices.question_id = questions.id
+    LEFT OUTER JOIN
+      (
+        SELECT
+          *
+        FROM
+          responses
+        WHERE
+          responses.user_id = ?
+      ) user_responses
+      ON user_responses.answer_choice_id = answer_choices.id
+    GROUP BY
+      polls.id
+    HAVING
+      COUNT(DISTINCT questions.id) = COUNT(user_responses.user_id)
+
+    ", self.id]
+  end
+
 end
+# #---
+# -- SELECT
+# --   polls.* --, questions.text question_text, questions.id question_ID
+# -- FROM
+# --   polls
+# -- JOIN
+# --   questions ON questions.poll_id = polls.id
+# -- JOIN
+# --   answer_choices ON answer_choices.question_id = questions.id
+# -- LEFT OUTER JOIN
+# --   responses ON responses.answer_choice_id = answer_choices.id
+# -- WHERE
+# --   responses.user_id = 1
+# -- GROUP BY
+# --   questions.id, answer_choices.id, responses.user_id
+# -- HAVING
+# --   COUNT(questions.id) = COUNT(responses.user_id)
+# -- #----
+# --
+# --
+# --
+# --
+# --
+# --
+# --
+# -- WHERE
+# --   (SELECT
+# --     COUNT(responses.id) AS questions_answered_count
+# --   FROM
+# --     polls
+# --   JOIN
+# --     questions ON questions.poll_id = polls.id
+# --   JOIN
+# --     answer_choices ON answer_choices.question_id = questions.id
+# --   LEFT OUTER JOIN
+# --     responses ON responses.answer_choice_id = answer_choices.id
+# --   JOIN
+# --     users ON users.id = responses.user_id
+# --   WHERE
+# --     responses.user_id = 2
+# --   AND
+# --     polls.id = 1
+# -- ) =
+# -- ( SELECT
+# --     COUNT(questions.id) AS questions_count
+# --   FROM
+# --     polls
+# --   LEFT OUTER JOIN
+# --     questions ON questions.poll_id = polls.id
+# --   WHERE
+# --     polls.id = 1
+# --
+# -- )
+# -- #----
+# -- SELECT
