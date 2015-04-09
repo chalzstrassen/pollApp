232eea8820c9f0ce58b1895e712e3dff9ac865ce
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-04-09 10:49:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollsApp::Application.config.secret_key_base = '5a9593680c3d4196666465851e46766bd58aba91396315caa1544f366d81fb2578fcec71ce83a76e542d0c96e2971e96bb883e4099f3f82aaf98900764ff1d8a'
