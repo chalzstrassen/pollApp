7ba07de308a4e145fd10d406e438f06951304be7
rhoen+charles <rhoen+charles@appacademy.io>
2015-04-09 11:08:09 -0400

    Migrate polls and add timestamps to users table.

---
+++ db/migrate/20150409150621_add_timestamps_to_users.rb
@@ -0,0 +1,6 @@
+class AddTimestampsToUsers < ActiveRecord::Migration
+  def change
+    add_timestamps(:users)
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409145051) do
+ActiveRecord::Schema.define(version: 20150409150621) do
+
+  create_table "polls", force: true do |t|
+    t.string   "title"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
-    t.string "user_name"
+    t.string   "user_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
